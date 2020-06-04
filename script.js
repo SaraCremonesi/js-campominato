ba77874c3ffdb68ad6377e96162d37cb81797a04
SaraCremonesi <saracrem00@gmail.com>
2020-06-04 22:05:44 +0200

    completamento esercizio

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-// Chiedo all'utente di inserire un numero da 1 a 100
-var numeroInserito = parseInt(prompt('Inserisci un numero da 1 a 100'));
-console.log('Il numero inserito è:' , numeroInserito);
-
 // Creo un ciclo while per generare 16 numeri casuali
 var arrayNumeriRandom = [];
 while (arrayNumeriRandom.length < 16) {
@@ -14,6 +10,30 @@ while (arrayNumeriRandom.length < 16) {
 };
 console.log('I numeri generati sono:' , arrayNumeriRandom);
 
+// Chiedo all'utente di inserire un numero da 1 a 100
+var i = 0;
+var numeroProibito = false;
+// Creo un ciclo while per reiterare la richiesta di un numero per un massimo di 16 volte
+while (i < 16 && numeroProibito === false) {
+  var numeroInserito = parseInt(prompt('Inserisci un numero da 1 a 100'));
+  console.log('Il numero inserito è:' , numeroInserito);
+  // Creo un ciclo if per verificare se il numero inserito compare nell'array (fine gioco),
+  // altrimenti incremento il contatore per calcolare successivamente il punteggio
+  if (verificaPresenza(numeroInserito, arrayNumeriRandom) === true) {
+    numeroProibito = true;
+  } else {
+    i++;
+  };
+};
+
+// Creo un ciclo if per stampare a video il messaggio
+if (numeroProibito === true) {
+  var messaggio = ('Fine del gioco. Il tuo punteggio è: ' + i);
+  console.log(messaggio);
+} else {
+  messaggio = ('Hai vinto!');
+  console.log(messaggio);
+};
 // **********FUNZIONI*********
 // Creo una funzione per verificare che non ci siano numeri ripetuti piu volte nell'array di numeri generati
 function verificaPresenza(numero, array) {
