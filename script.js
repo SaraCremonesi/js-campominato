8bddd4416a286b5817e751211e7b5fdecb99d128
SaraCremonesi <saracrem00@gmail.com>
2020-06-04 17:09:28 +0200

    aggiornamento

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
-
 // Chiedo all'utente di inserire un numero da 1 a 100
 var numeroInserito = parseInt(prompt('Inserisci un numero da 1 a 100'));
-while (numeroInserito === '' || numeroInserito < 1 || numeroInserito > 100) {
-  numeroInserito = parseInt(prompt('Errore. Inserisci un numero da 1 a 100'));
-};
 console.log('Il numero inserito è:' , numeroInserito);
 
-// Creo un ciclo for per generare dei numeri casuali da 1 a 100 ed inserirli in un array
-var numeriRandom = [];
-for (var i = 0; i < 16; i++) {
+// Creo un ciclo while per generare 16 numeri casuali
+var arrayNumeriRandom = [];
+while (arrayNumeriRandom.length < 16) {
+  // Genero i 16 numeri casuali tra 1 e 100
   var numeroGenerato = Math.floor(Math.random() * 100 + 1);
-  numeriRandom.push(numeroGenerato);
-};
-console.log('I numeri generati sono:' , numeriRandom);
-
-for (var i = 0; i < 16; i++) {
-  if (numeroInserito != numeriRandom[i]) {
-    numeroInserito = parseInt(prompt('Sei ancora in gioco. Inserisci un altro numero da 1 a 100'));
-  } else {
-    var messaggio = 'Hai perso';
-    console.log(messaggio);
+  // Invoco la funzione per la verifica dei doppioni e la valido in un if per poter inserire i numeri nell'array
+  if (verificaPresenza(numeroGenerato, arrayNumeriRandom) === false) {
+    arrayNumeriRandom.push(numeroGenerato);
   };
+};
+console.log('I numeri generati sono:' , arrayNumeriRandom);
+
+// **********FUNZIONI*********
+// Creo una funzione per verificare che non ci siano numeri ripetuti piu volte nell'array di numeri generati
+function verificaPresenza(numero, array) {
+  var presenza = false;
+  for (var i = 0; i < 16; i++) {
+    if (numero === array[i]) {
+      presenza = true;
+    };
+  };
+  return presenza;
 };
